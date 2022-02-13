9d9b7e7ee53ac1e2059b4e761d7c36d7ca96316c
Brielle Hina <bhina@udel.edu>
2022-02-13 17:06:48 -0500

    Added hello world to app

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Brielle Hina
+                automatically reload. Brielle Hina. Hello World
             </p>
         </div>
     );
