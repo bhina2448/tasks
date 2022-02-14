ef28a0527581aa83a025c34f6d850bdab1fb7503
Brielle Hina <bhina@udel.edu>
2022-02-14 08:49:23 -0500

    Lab ended

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -11,6 +13,26 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Brielle Hina. Hello World
             </p>
+            <h1>This is my header</h1>
+            <Container>
+                <Row>
+                    <Col>
+                        <img src="Diesel.JPG" alt="Old Man Diesel" />
+                        pets
+                    </Col>
+                    <Col>
+                        <ul>
+                            <li>Diesel</li>
+                            <li>Bryson</li>
+                            <li>Roni</li>
+                            <li>Tigger</li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
         </div>
     );
 }
