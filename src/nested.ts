7786ad22e5dd4ef937d57999985f6a6c45140d3b
Brielle Hina <bhina@udel.edu>
2022-03-13 20:01:12 -0400

    completed toCSV

--- src/nested.ts
+++ src/nested.ts
@@ -100,7 +100,20 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const header = "id,name,options,points,published\n";
+    const body = questions
+        .map(
+            (question: Question): string =>
+                question.id +
+                "," +
+                question.name +
+                "," +
+                question.options.length +
+                "," +
+                question.published
+        )
+        .join("\n");
+    return header + body;
 }
 
 /**
