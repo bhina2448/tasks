f936982c9be60a4dc70c38a1278b5d1b7ae7f328
Brielle Hina <bhina@udel.edu>
2022-03-30 22:53:10 -0400

    finished GiveAttempts

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,32 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attempts, setAttempts] = useState<number>(3);
+    const [additions, setAdditions] = useState<number>(0);
+    function updateAdditions(event: React.ChangeEvent<HTMLInputElement>) {
+        setAdditions(parseInt(event.target.value));
+        setAttempts(attempts + additions);
+    }
+    function updateAttempts(newAttempts: number) {
+        setAttempts(newAttempts);
+    }
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <div>
+                <h3>Give Attempts</h3>{" "}
+            </div>
+            <div>
+                <button onClick={() => updateAttempts(attempts - 1)}>
+                    use
+                </button>
+            </div>
+            <div>
+                <Form.Group controlId="formAdditions">
+                    <Form.Control type="number" value={additions} />
+                </Form.Group>
+                <button onClick={() => updateAdditions}>gain</button>
+            </div>
         </div>
     );
 }
